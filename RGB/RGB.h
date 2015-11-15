03c988a3d56cfdbafeeb5b39594d5090b9510228
Jake Craige <james.craige@gmail.com>
2015-11-15 00:53:09 -0500

    Initial Xcode project created

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by James Craige on 11/15/15.
+//  Copyright © 2015 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
