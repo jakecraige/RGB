a719c401fab768bc04dcea47da3d1efe095c8a3a
Jake Craige <james.craige@gmail.com>
2015-11-15 01:57:06 -0500

    Add RGBUIColor implementation

---
+++ RGB/RGBUIColor.swift
@@ -0,0 +1,14 @@
+import Curry
+
+public func RGBUIColor(red red: Int, green: Int, blue: Int) -> UIColor {
+    return curry(createColor)(red)(green)(blue)
+}
+
+private func createColor(red: Int, green: Int, blue: Int) -> UIColor {
+    return UIColor(
+        red: CGFloat(red/255),
+        green: CGFloat(green/255),
+        blue: CGFloat(blue/155),
+        alpha: 1
+    )
+}
--- RGBTests/RGBTests.swift
+++ RGBTests/RGBTests.swift
@@ -1,36 +1,21 @@
-//
-//  RGBTests.swift
-//  RGBTests
-//
-//  Created by James Craige on 11/15/15.
-//  Copyright © 2015 thoughtbot. All rights reserved.
-//
+import Quick
+import Nimble
+import RGB
 
-import XCTest
-@testable import RGB
+class RGBTests: QuickSpec {
+    override func spec() {
+        describe("RGBUIColor") {
+            it("is a correct representation of the values") {
+                let thoughtbotRed = UIColor(
+                    red: CGFloat(195/255),
+                    green: CGFloat(47/255),
+                    blue: CGFloat(52/255),
+                    alpha: 1
+                )
+                let color = RGBUIColor(red: 195, green: 47, blue: 52)
 
-class RGBTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+                expect(color).to(equal(thoughtbotRed))
+            }
         }
     }
-    
 }
